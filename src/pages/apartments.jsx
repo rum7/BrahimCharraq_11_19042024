4b5c5240e29dc7a24aadad91792e3b63232be849
Brahim <87144903+rum7@users.noreply.github.com>
2024-05-15 16:20:24 +0200

    fix apartments wrong id redirection
    When an apartment's link contains a wrong id, user is redirected to the home page using "Navigate" tool from react-router-dom.

--- src/pages/apartments.jsx
+++ src/pages/apartments.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, Navigate } from "react-router-dom"
 import ApartmentsData from "@/data"
 
 import { Carousel } from "@/components"
@@ -14,46 +14,50 @@ export const Apartments = () => {
     const data = Object.values(ApartmentsData())
     const currentApartmentData = data.filter(apartment => apartment.id === apartmentId)
 
-    return (
-        <main>
-            <section className="apartment">
-                <Carousel
-                    galleryTitle={currentApartmentData[0].title}
-                    apartmentPictures={currentApartmentData[0].pictures}
-                />                    
-                
-                <div className="apartment-details">
-                    <div className="apartment-details__info">
-                        <h2>{currentApartmentData[0].title}</h2>
-                        <h3>{currentApartmentData[0].location}</h3>
-                        <ul>
-                            {currentApartmentData[0].tags.map((item, index) =>
-                                <li key={index}>
-                                    <p>{item}</p>
-                                </li>
-                            )}
-                        </ul>
-                    </div>
-                    <div className="apartment-details__host">
-                        <div className="host-info">
-                            <p>{currentApartmentData[0].host.name.replace(/\s/g, '\n')}</p>
-                            <img src={currentApartmentData[0].host.picture} alt={currentApartmentData[0].host.name} />
+    if (!currentApartmentData[0]) {
+        return (<Navigate to="/" />)
+    } else {
+        return (
+            <main>
+                <section className="apartment">
+                    <Carousel
+                        galleryTitle={currentApartmentData[0].title}
+                        apartmentPictures={currentApartmentData[0].pictures}
+                    />                    
+                    
+                    <div className="apartment-details">
+                        <div className="apartment-details__info">
+                            <h2>{currentApartmentData[0].title}</h2>
+                            <h3>{currentApartmentData[0].location}</h3>
+                            <ul>
+                                {currentApartmentData[0].tags.map((item, index) =>
+                                    <li key={index}>
+                                        <p>{item}</p>
+                                    </li>
+                                )}
+                            </ul>
                         </div>
-                        <Rating rating={currentApartmentData[0].rating} />
+                        <div className="apartment-details__host">
+                            <div className="host-info">
+                                <p>{currentApartmentData[0].host.name.replace(/\s/g, '\n')}</p>
+                                <img src={currentApartmentData[0].host.picture} alt={currentApartmentData[0].host.name} />
+                            </div>
+                            <Rating rating={currentApartmentData[0].rating} />
+                        </div>
                     </div>
-                </div>
-
-                <div className="apartment-more">
-                    <Collapse
-                        collapseTitle={"Description"}
-                        collapseContent={currentApartmentData[0].description}
-                    />
-                    <Collapse
-                        collapseTitle={"Équipements"}
-                        collapseContent={currentApartmentData[0].equipments}
-                    />
-                </div>
-            </section>
-        </main>
-    )
+    
+                    <div className="apartment-more">
+                        <Collapse
+                            collapseTitle={"Description"}
+                            collapseContent={currentApartmentData[0].description}
+                        />
+                        <Collapse
+                            collapseTitle={"Équipements"}
+                            collapseContent={currentApartmentData[0].equipments}
+                        />
+                    </div>
+                </section>
+            </main>
+        )
+    }   
 }
